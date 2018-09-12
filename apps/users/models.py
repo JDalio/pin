4afcaa48e57e50a7152d0564ab3fcb7085bc9dad
Dalio <2267264176@qq.com>
2018-09-13 01:00:46 +0800

    开始创建Model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    openid=models.CharField(max_length=64,verbose_name="用户id")
+    nick_name=models.CharField(max_length=20,verbose_name="昵称")
+    phone=models.CharField(max_length=11,verbose_name="手机号")
+    address=models.CharField(choices=[])
+    gender=models.BooleanField()
