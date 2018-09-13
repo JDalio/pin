e2eb1cedd8b12e6432521a4035f5433629066b97
Dalio <2267264176@qq.com>
2018-09-14 00:36:39 +0800

    开始用户登录

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class Users(AbstractBaseUser):
+    avatar_url = models.URLField(verbose_name="头像路径")
     openid = models.CharField(max_length=64, unique=True, verbose_name="用户id")
     nick_name = models.CharField(max_length=30, verbose_name="昵称")
     phone = models.CharField(max_length=11, unique=True, verbose_name="手机号")
     gender = models.BooleanField(default=True, verbose_name="为女生？")
+    province=models.CharField(max_length=20,verbose_name='省份')
+    city=models.CharField(max_length=20,verbose_name='城市')
     address = models.CharField(
         choices=[('学1', '学1'), ('学2', '学2'), ('学3', '学3'), ('学4', '学4'), ('学5', '学5'), ('学6', '学6'), ('学8', '学8'),
                  ('学9', '学9'), ('学11', '学11'), ('学13', '学13'), ('学29', '学29'), ('留西', '留西'), ('留东', '留东'),
